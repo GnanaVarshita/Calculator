58040f602a46a985832e678b69b6e380692bb846
GnanaVarshita <gnanavarshitagv@gmail.com>
2023-03-16 18:45:26 +0530

    Uploading Files

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1=8;
+let num2=2;
+
+document.getElementById(num1-el).textContent=num1;
+document.getElementById(num2-el).textContent=num2;
+
+let  = document.getElementById("res");
+function Add(){
+
+    result=num1+num1;
+
+}
